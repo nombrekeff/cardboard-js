1de57233d2f402f0984e8f4b9d722a33570ab1e7
Manolo Edge <nombrekeff@rallf.com>
2023-10-14 18:09:31 +0200

    docs: added docs for state

--- dist/state.d.ts
+++ dist/state.d.ts
@@ -1,2 +1,25 @@
 import { State } from './types.js';
+/**
+ * `state` creates a reactive object that can the be used with tags to create dinamic and reactive apps.
+ * {@param content} can be an `object` or an `array`. Objects can be nested, and evey property will be reactive.
+ * In arrays, length will also be reactive.
+ *
+ * You can pass an optional {@param callback}, that will be called anything in the state changes.
+ *
+ * Additionally you can listen to it after creating it: `state().changed(() => { })`
+ *
+ * @example
+ * ```ts
+ * const st = state({ count: 0 });
+ * st.changed(() => { ... });
+ * st.count.changed(() => { ... });
+ *
+ * st.count++;
+ * st.count = 3;
+ *
+ * div().hideIf(st.count);
+ * div().disableIf(st.count);
+ * div(template('Count is: $count', st));
+ * ```
+ */
 export declare function state<T extends object>(content: T, callback?: (newValue: T) => void): State<T>;
--- dist/state.js
+++ dist/state.js
@@ -1,4 +1,27 @@
 import { isObject } from './util.js';
+/**
+ * `state` creates a reactive object that can the be used with tags to create dinamic and reactive apps.
+ * {@param content} can be an `object` or an `array`. Objects can be nested, and evey property will be reactive.
+ * In arrays, length will also be reactive.
+ *
+ * You can pass an optional {@param callback}, that will be called anything in the state changes.
+ *
+ * Additionally you can listen to it after creating it: `state().changed(() => { })`
+ *
+ * @example
+ * ```ts
+ * const st = state({ count: 0 });
+ * st.changed(() => { ... });
+ * st.count.changed(() => { ... });
+ *
+ * st.count++;
+ * st.count = 3;
+ *
+ * div().hideIf(st.count);
+ * div().disableIf(st.count);
+ * div(template('Count is: $count', st));
+ * ```
+ */
 export function state(content, callback) {
     let _propListeners = {};
     let _stateListeners = [];
