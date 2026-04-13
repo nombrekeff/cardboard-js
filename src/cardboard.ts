98eb8bd7a7ebc683496917e662c7f953cc7ccb39
manolo edge <manoloedge@manolos-MacBook-Pro.local>
2026-04-13 16:30:40 +0200

    feat: restructure exports and initialize framework in core.ts

--- src/cardboard.ts
+++ src/cardboard.ts
@@ -1,39 +1,7 @@
-import * as _context from './context.js';
-import * as _tag from './tag.js';
-import * as _styles from './style-manager.js';
-
-export * from './context.js';
-export * from './tag.js';
-export * from './state.js';
-export * from './css-generator.js';
-export * from './util.js';
-export * from './text.js';
-export * from './events.js';
-export * from './each.js';
-export * from './lifecycle.js';
-export * from './observables.js';
-export * from './all-tags.js';
+export * from './core.js';
 
 export * from './ext/reactivity/chaining.js';
 export * from './ext/reactivity/reactivity.js';
 
 export * from './ext/css/chaining.js';
 export * from './ext/css/styling.js';
-
-export type * from './types';
-
-
-/**
- * It initializes the framework & makes the body tag the mount point ({@link mountPoint}).
- * You can pass in a selector for an element you want to be the default tag ("body" by default).
- */
-export const init = (options: { selector: string } = { selector: 'body' }) => {
-  _context.context.init = true;
-  _context.context.obs = _context.createGlobalObserver();
-  _context.context.styleManager = new _styles.StyleManager();
-
-  const tag = new _tag.CTag(`(${options.selector})`);
-  return _context.mountPoint(tag);
-};
-
-export const version = '0.1.0'; // This should be replaced with the actual version from package.json during the build process
